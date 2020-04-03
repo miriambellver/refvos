24d120705de2f04ea64e0bd38f067644b2058247
miriambellver <miriambellver@users.noreply.github.com>
2020-04-03 12:22:09 +0200

    Update args.py

--- args.py
+++ args.py
@@ -2,11 +2,11 @@ import argparse
 
 
 def get_parser():
-    parser = argparse.ArgumentParser(description='PyTorch Segmentation Training')
+    parser = argparse.ArgumentParser(description='RefVOS Training')
 
     parser.add_argument('--model_id', default='my_model', help='name to identify model')
 
-    parser.add_argument('--dataset', default='refcoco', help='the different options are refcoco, davis or a2d')
+    parser.add_argument('--dataset', default='refcoco', help='choose one of the following datasets: refcoco, refcoco+, davis or a2d')
     parser.add_argument('--model', default='deeplabv3_resnet101', help='model')
 
     parser.add_argument('--aux-loss', action='store_true', help='auxiliar loss')
@@ -31,7 +31,7 @@ def get_parser():
 
     parser.add_argument('--optimizer', default='sgd')
     parser.add_argument('--print-freq', default=10, type=int, help='print frequency')
-    parser.add_argument('--output-dir', default='/gpfs/scratch/bsc31/bsc31429/dev/vog/baseline_refcoco/checkpoints/', help='path where to save checkpoints')
+    parser.add_argument('--output-dir', default='./checkpoints/', help='path where to save checkpoints')
 
     parser.add_argument("--local_rank", type=int)
     parser.add_argument('--device', default='cuda', help='device')
@@ -39,14 +39,14 @@ def get_parser():
     parser.add_argument("--test-only", dest="test_only", help="Only test the model", action="store_true",)
 
     # Fusion language + visual
-    parser.add_argument('--multiply_feats', action='store_true', default=True, help='multiplication of features instead of concatanation')
-    parser.add_argument('--addition',  action='store_true', help='residual connection when features fusion')
+    parser.add_argument('--multiply_feats', action='store_true', default=True, help='multiplication of visual and language features')
+    parser.add_argument('--addition',  action='store_true', help='addition of visual and language features')
 
     # Learning rate strategies
-    parser.add_argument('--fixed_lr', action='store_true', help='whether to fix the learning rate')
-    parser.add_argument('--linear_lr', action='store_true', help='lr scheduler 5')
-    parser.add_argument('--lr_specific', default=0.00003, type=float, help='specific lr for fixed configuration')
-    parser.add_argument('--lr_specific_decrease', default=0.001, type=float, help='specific lr for fixed configuration')
+    parser.add_argument('--fixed_lr', action='store_true', help='use fixed learning rate')
+    parser.add_argument('--linear_lr', action='store_true', help='use linear learning rate schedule')
+    parser.add_argument('--lr_specific', default=0.00003, type=float, help='specific lr for fixed lr configuration')
+    parser.add_argument('--lr_specific_decrease', default=0.001, type=float, help='specific lr decrease for linear lr configuration')
     
     
     #### Baseline 
@@ -56,39 +56,40 @@ def get_parser():
     #### Training configurations
     parser.add_argument('--load_optimizer', action='store_true', help='load optimizer')
     parser.add_argument('--resume', default='', help='resume from checkpoint')
-    parser.add_argument('--bert_tokenizer',  default='/gpfs/scratch/bsc31/bsc31429/dev/vog/pytorch-transformers/bert-base-uncased-vocab.txt', help='tokenizer BERT')
-    parser.add_argument('--glove_dict',  default='/gpfs/scratch/bsc31/bsc31429/dev/vog/glove.840B.300d.txt', help='glove dict')
-    parser.add_argument('--ck_bert',  default='/gpfs/scratch/bsc31/bsc31429/dev/vog/pytorch-transformers/pretrained_models/', help='which CK of BERT to consider')
+    parser.add_argument('--bert_tokenizer',  default='bert-base-uncased', help='BERT tokenizer')
+    parser.add_argument('--glove_dict',  default='./glove.840B.300d.txt', help='glove dict that you need to download and save')
+    parser.add_argument('--ck_bert',  default='bert-base-uncased', help='BERT pre-trained weights')
 
 
     #### Testing parameters
-    parser.add_argument('--results_folder',  default='/gpfs/scratch/bsc31/bsc31429/dev/refvos/results/', help='glove dict')
-    parser.add_argument('--submission_path',  default='/gpfs/scratch/bsc31/bsc31429/dev/refvos/results_davis_full_video_ann1_submission/', help='glove dict')
+    parser.add_argument('--results_folder',  default='./results/', help='results folder')
+    parser.add_argument('--submission_path',  default='./results_submission/', help='submission results folder for DAVIS')
     parser.add_argument('--split',  default='test', help='split to run test')
+    parser.add_argument('--display',  action='store_true', help='save output predictions')
 
     #### Dataset specifics
 
     # pretraining
-    parser.add_argument("--pretrained_refcoco", dest="pretrained_refcoco", help="Use pre-trained models from refcoco", action="store_true",)
-    parser.add_argument('--ck_pretrained_coco',  default='/gpfs/scratch/bsc31/bsc31429/dev/vog/baseline_refcoco/checkpoints/model_refcoco_v4_bert_all_encoder_14.pth', help='which CK pretrained on refcoco to consider')
+    parser.add_argument("--pretrained_refvos", dest="pretrained_refvos", help="Use pre-trained models for RefVOS", action="store_true",)
+    parser.add_argument('--ck_pretrained_refvos',  default='./checkpoints/model_refcoco.pth', help='Pre-trained weights for RefVOS')
 
     # REFER
-    parser.add_argument('--data_root', default='/gpfs/scratch/bsc31/bsc31429/dev/vog/datasets/refer/data/', help='dataset root directory')
+    parser.add_argument('--refer_data_root', default='./datasets/refer/data/', help='REFER dataset root directory')
     parser.add_argument('--refer_dataset', default='refcoco', help='dataset name')
     parser.add_argument('--splitBy', default='unc', help='split By')
 
     # DAVIS
-    parser.add_argument('--emb_type', default='first_mask', help='first_mask or full_video for davis')
-    parser.add_argument('--davis_data_root', default='/gpfs/scratch/bsc31/bsc31429/databases/davis2017', help='davis dataset root directory')
-    parser.add_argument('--davis_annotations_file', default='/gpfs/scratch/bsc31/bsc31429/databases/davis2017/davis_text_annotations/Davis17_annot1_full_video.txt', help='path of annotations file')
+    parser.add_argument('--davis_data_root', default='./datasets/davis2017', help='DAVIS dataset root directory')
+    parser.add_argument('--davis_annotations_file', default='./datasets/davis2017/davis_text_annotations/Davis17_annot1_full_video.txt', help='path of DAVIS annotations file')
+    parser.add_argument('--emb_type', default='first_mask', help='first_mask or full_video for DAVIS')
 
     # A2D
-    parser.add_argument('--a2d_root_dir', default='/gpfs/scratch/bsc31/bsc31429/databases/Release/')
-    parser.add_argument('--size_a2d_x', default=240, type=int, help='x size for a2d images')
-    parser.add_argument('--size_a2d_y', default=427, type=int, help='y size for a2d images')
-    parser.add_argument('--a2d_annotations_file', default='/gpfs/scratch/bsc31/bsc31429/databases/Release/a2d_annotation.txt',  help='path of annotations file')
+    parser.add_argument('--a2d_data_root', default='./datasets/Release/', help='A2D dataset root directory')
+    parser.add_argument('--size_a2d_x', default=240, type=int, help='x size for A2D images')
+    parser.add_argument('--size_a2d_y', default=427, type=int, help='y size for A2D images')
+    parser.add_argument('--a2d_annotations_file', default='./datasets/Release/a2d_annotation.txt',  help='path of A2D annotations file')
 
-
+    
     return parser
 
 
